2cca970668b4e83486eac0261e407e773a2d7697
Joseph Felicidario <josephflec@gmail.com>
2023-10-07 00:17:11 -0500

    Initial commit

---
+++ src/main/java/pinoypicnic/PinoyPicnicClient.java
@@ -0,0 +1,10 @@
+package pinoypicnic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PinoyPicnicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
